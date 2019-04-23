091b75f89cc751888505c7a31fa53a638d2fd96b
Gonzalo Pascual <gonzalopascual.gpc@gmail.com>
2019-04-23 23:59:09 +0200

    kata 04 >> bonus >> reduce code duplicity by creating the inventory instance in a setUp() function

--- katas_java/04-working-with-code-we-do-not-own/src/test/java/GildedRoseTest.java
+++ katas_java/04-working-with-code-we-do-not-own/src/test/java/GildedRoseTest.java
@@ -1,4 +1,4 @@
-import Products.ProductSupplier;
+import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -6,9 +6,16 @@ import Products.Product;
 import static Products.ProductSupplier.*;
 
 public class GildedRoseTest {
+
+  private Inventory inventory;
+
+  @Before
+  public void setUp() {
+    inventory = new Inventory();
+  }
+
   @Test
   public void given_an_product_when_sell_in_approaches_then_quality_decreases() {
-    Inventory inventory = new Inventory();
     Product product = askFor("fromage", 20, 20);
     inventory.addProduct(product);
 
@@ -22,7 +29,6 @@ public class GildedRoseTest {
 
   @Test
   public void given_an_product_with_passed_sell_in_when_a_day_passes_then_quality_decreases_2x_faster() {
-    Inventory inventory = new Inventory();
     Product product = askFor("fromage", -2, 20);
     inventory.addProduct(product);
 
@@ -36,7 +42,6 @@ public class GildedRoseTest {
 
   @Test
   public void given_aged_brie_when_a_day_passes_then_quality_increases() {
-    Inventory inventory = new Inventory();
     Product product = askForAgedBrie(20, 20);
     Product otherBrie = askFor("Aged Brie", 20, 20);
     inventory.addProduct(product);
@@ -54,7 +59,6 @@ public class GildedRoseTest {
 
   @Test
   public void given_a_product_without_any_quality_left_when_a_day_passes_then_its_quality_remains_zero() {
-    Inventory inventory = new Inventory();
     Product product = askFor("fromage", 2, 0);
     inventory.addProduct(product);
 
@@ -66,7 +70,6 @@ public class GildedRoseTest {
 
   @Test
   public void given_a_product_whose_quality_is_50_when_a_day_passes_then_its_quality_doesnt_increase_anymore() {
-    Inventory inventory = new Inventory();
     Product product = askForAgedBrie(2, 50);
     inventory.addProduct(product);
 
@@ -78,7 +81,6 @@ public class GildedRoseTest {
 
   @Test
   public void given_a_legendary_product_when_a_day_passes_then_its_quality_remains_80() {
-    Inventory inventory = new Inventory();
     Product product = askForSulfuras();
     Product otherSulfuras = askFor("Sulfuras", 20, 20);
     inventory.addProduct(product);
@@ -93,7 +95,6 @@ public class GildedRoseTest {
 
   @Test
   public void given_a_legendary_product_when_a_day_passes_then_its_sell_in_is_null() {
-    Inventory inventory = new Inventory();
     Product product = askForSulfuras();
     inventory.addProduct(product);
 
@@ -105,8 +106,7 @@ public class GildedRoseTest {
 
   @Test
   public void given_backstage_passes_when_a_day_passes_then_its_quality_increases() {
-    Inventory inventory = new Inventory();
-    Product product = ProductSupplier.askForBackstagePasses(20, 20);
+    Product product = askForBackstagePasses(20, 20);
     Product otherBackstagePasses = askFor("Backstage passes", 20, 20);
     inventory.addProduct(product);
     inventory.addProduct(otherBackstagePasses);
@@ -123,7 +123,6 @@ public class GildedRoseTest {
 
   @Test
   public void given_backstage_passes_when_there_are_10_or_less_days_to_sell_in_then_the_quality_increases_by_2() {
-    Inventory inventory = new Inventory();
     Product product = askForBackstagePasses(11, 20);
     Product otherBackstagePasses = askForBackstagePasses(10, 20);
     inventory.addProduct(product);
@@ -138,7 +137,6 @@ public class GildedRoseTest {
 
   @Test
   public void given_backstage_passes_when_there_are_5_or_less_days_to_sell_in_then_the_quality_increases_by_3() {
-    Inventory inventory = new Inventory();
     Product product = askForBackstagePasses(6, 20);
     Product otherBackstagePasses = askForBackstagePasses(5, 20);
     inventory.addProduct(product);
@@ -153,7 +151,6 @@ public class GildedRoseTest {
 
   @Test
   public void given_backstage_passes_when_there_sell_in_has_passed_then_the_quality_is_0() {
-    Inventory inventory = new Inventory();
     Product missedBackstagePasses = askForBackstagePasses(0, 20);
     inventory.addProduct(missedBackstagePasses);
 
@@ -165,8 +162,7 @@ public class GildedRoseTest {
 
   @Test
   public void given_a_conjured_product_when_a_day_passes_then_its_quality_decreases_2x_faster() {
-    Inventory inventory = new Inventory();
-    Product product = ProductSupplier.askForConjured(20, 20);
+    Product product = askForConjured(20, 20);
     Product otherConjuredProduct = askFor("Conjured", 0, 20);
     inventory.addProduct(product);
     inventory.addProduct(otherConjuredProduct);
